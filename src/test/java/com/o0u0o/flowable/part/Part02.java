a6a135df9bf166640ddeda185e3187a95c62bb77
o0u0o <1553122309@qq.com>
2023-03-13 09:38:56 +0800

    新增bpmn文件

--- src/test/java/com/o0u0o/flowable/part/Part02.java
+++ src/test/java/com/o0u0o/flowable/part/Part02.java
@@ -23,18 +23,20 @@ public class Part02 extends FlowableHiApplicationTests {
     public void testRunProcess(){
         ProcessEngine processEngine = configuration.buildProcessEngine();
 
-        //1. 我们需要启动RuntimeService来启动流程实例
+        //1. 我们需要启动 RuntimeService 来启动流程实例
         RuntimeService runtimeService = processEngine.getRuntimeService();
 
-        //2.构建流程变量
+        //2.构建流程变量(业务中的表单数据)
         Map<String, Object> variables = new HashMap<>();
         variables.put("employee", "张三");
         variables.put("nrOfHolidays", 3);
         variables.put("description", "工作累了！出去玩玩！");
-        ProcessInstance holidayRequest = runtimeService.startProcessInstanceByKey("请假流程", variables);
+        ProcessInstance holidayRequest = runtimeService.startProcessInstanceByKey("holidayRequest", variables);
+        //3.启动流程实例
+        //ProcessInstance holidayRequest = runtimeService.startProcessInstanceById("3:7503", variables);
 
         System.out.println("holidayRequest.getDeploymentId:" + holidayRequest.getDeploymentId());
-
+        System.out.println("holidayRequest.getName:" + holidayRequest.getName());
 
         //
     }
