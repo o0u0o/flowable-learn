f99eab01b2f74a7fba8ecaca150fe781e92541d0
o0u0o <1553122309@qq.com>
2023-03-15 22:18:42 +0800

    激活和挂起流程

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 资源管理
 ![](https://images.techgeng.com/blog/%E6%88%AA%E5%B1%8F2023-03-10%20%E4%B8%8B%E5%8D%8810.53.49.png)
 
+# 流程部署
+流程会涉及到3张表：
+
+    - 流程部署表：ACT_RE_DEPLOYMENT 一次流程部署操作，就会生成一张表结构 
+
+    - 流程定义表：ACT_RE_PROCDEF 一次部署操作中，包含几个流程定义文件就会产生几条记录
+
+    - 流程定义资源文件表：ACT_GE_BYTEARRAY
---
+++ src/test/java/com/o0u0o/flowable/part/Part04.java
@@ -0,0 +1,40 @@
+package com.o0u0o.flowable.part;
+import org.flowable.engine.ProcessEngine;import org.flowable.engine.ProcessEngines;import org.flowable.engine.RepositoryService;import org.flowable.engine.repository.ProcessDefinition;import org.flowable.engine.repository.ProcessDefinitionQuery;import org.junit.Test; /**
+ * <h1></h1>
+ * @author o0u0o
+ * @date 2023/3/15 9:59 PM
+ */
+public class Part04 {
+
+    /**
+     * <h2>流程的挂起与激活</h2>
+     */
+    @Test
+    public void testSuspended(){
+        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+        RepositoryService repositoryService = engine.getRepositoryService();
+
+        //获取对应的流程定义信息
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionId("holiday:1:4")
+                .singleResult();
+
+        //获取当前的流程定义的状态信息
+        boolean suspended = processDefinition.isSuspended();
+        if (suspended){
+            //当前的流程被 挂起了
+            //激活流程
+            System.out.println("激活流程：" + processDefinition.getId() + processDefinition.getName());
+            repositoryService.activateProcessDefinitionById("holiday:1:4");
+        }
+        else {
+            //当前的流程是激活状态
+            System.out.println("挂起流程：" + processDefinition.getId() + processDefinition.getName());
+            //我们可以挂起当前的流程
+            repositoryService.suspendProcessDefinitionByKey("holiday:1:4");
+
+        }
+
+    }
+
+}
