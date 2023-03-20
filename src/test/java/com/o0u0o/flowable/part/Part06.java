be1a724e5f6acf70b7071064d6dd0f5dd2309c2a
o0u0o <1553122309@qq.com>
2023-03-20 13:51:16 +0800

    请假流程，候选人

--- src/test/java/com/o0u0o/flowable/part/Part06.java
+++ src/test/java/com/o0u0o/flowable/part/Part06.java
@@ -1,5 +1,11 @@
 package com.o0u0o.flowable.part;
-import com.o0u0o.flowable.FlowableHiApplicationTests;import org.flowable.engine.ProcessEngine;import org.flowable.engine.RepositoryService;import org.flowable.engine.repository.Deployment;import org.junit.Test;
+import com.o0u0o.flowable.FlowableHiApplicationTests;
+import org.flowable.engine.*;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
 /**
  * <h1>流程变量</h1>
  * @author o0u0o
@@ -23,7 +29,94 @@ public class Part06 extends FlowableHiApplicationTests {
                 //部署流程
                 .deploy();
 
-        System.out.println("deploy.getId:" + deploy.getId());
-        System.out.println("deploy.getName:" + deploy.getName());
+        System.out.println("deploy.getId(流程部署ID):" + deploy.getId());
+        System.out.println("deploy.getName((流程部署名):" + deploy.getName());
+    }
+
+    /**
+     * <h1>启动流程实例</h1>
+     */
+    @Test
+    public void runProcess(){
+        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+        RuntimeService runtimeService = engine.getRuntimeService();
+
+        //流程定义ID
+        String processDefinitionId = "evection:1:4";
+        //在启动流程实例的时候创建了流程变量（定义了执行人是谁）
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("assignee0", "张三");
+        variables.put("assignee1", "李四");
+        variables.put("assignee2", "王五");
+        variables.put("assignee3", "赵会计");
+
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinitionId, variables);
+        System.out.println("processInstance = " + processInstance);
+        System.out.println("processInstance.getProcessDefinitionId() = " + processInstance.getProcessDefinitionId());
+
+    }
+
+    @Test
+    public void completeTask(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        Task task = taskService.createTaskQuery()
+                .processInstanceId("2501")
+                //张三去处理这个任务
+                .taskAssignee("张三")
+                .singleResult();
+
+        //获取当前流程实例所有的 已有的流程变量
+        Map<String,Object> processVariables = task.getProcessVariables();
+        //新设置一个流程变量
+        processVariables.put("num", 2);
+        taskService.complete(task.getId(), processVariables);
+    }
+
+    /**
+     * <h2>根据Task编号来更新流程变量</h2>
+     * 举例：一开始我申请出差2天，但是因为疫情原因，需要多出差2天
+     */
+    @Test
+    public void updateVariables(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+
+        //现在已经流转到李四这边了
+        Task task = taskService.createTaskQuery()
+                .processInstanceId("2501")
+                .taskAssignee("李四")
+                .singleResult();
+        //在局部变量和全局变量都有的情况下，这里取出来的就是局部变量
+        Map<String,Object> processVariables = task.getProcessVariables();
+        processVariables.put("num", 8);
+        //设置了本地（局部）变量
+        //taskService.setVariablesLocal(task.getId(), processVariables);
+
+        //设置更新全局变量
+        taskService.setVariables(task.getId(), processVariables);
+
+    }
+
+    @Test
+    public void completeTask1(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        Task task = taskService.createTaskQuery()
+                .processInstanceId("2501")
+                //张三去处理这个任务
+                .taskAssignee("李四")
+                .singleResult();
+
+        Map<String,Object> processVariables = task.getProcessVariables();
+        Object num = processVariables.get("num");
+//        类似于
+//        if (num >= 3){
+//            //......
+//        } else {
+//            //......
+//        }
+
+        taskService.complete(task.getId());
     }
 }
